6c20915867378b468b8847d34a28f6fdb24877e5
Benjamin Morrison <ben@gbmor.org>
2022-01-25 22:30:22 -0500

    script to enable touchpad tapping / tap-to-click

---
+++ bin/touchpad_enable_tap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+# This enables tapping, like tap-to-click, on the Thinkpad T14s Gen2 AMD.
+# The touchpad ID changes between reboots sometimes.
+#
+# We need property 318, "Tapping Enabled".
+# Available properties viewable with:
+#       xinput list-props $TOUCHPAD_ID
+#
+# Requires:
+#       xinput, libinput
+
+TOUCHPAD_ID="$(xinput | grep Touchpad | awk '{print $6}' | cut -d'=' -f2)"
+
+/usr/bin/xinput set-prop "$TOUCHPAD_ID" 318 1
